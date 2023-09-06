d15d77224206de274bb4c5053bdc16e051b8fe63
Aditya Krishna <adkris1002@gmail.com>
2023-09-05 23:05:10 -0700

    Added frequency threshold notebook for clustering intro

--- src/core.py
+++ src/core.py
@@ -23,12 +23,22 @@ FREQ_GROUPS = {
                 }
 
 EXAMPLE_FILES_to_FILEPATHS = {
+                            'UBNA_010/20220826_043000' : f'../data/audiomoth_recordings/recover-20220828/UBNA_010/20220826_043000.WAV',
+                            'UBNA_010/20220826_050000' : f'../data/audiomoth_recordings/recover-20220828/UBNA_010/20220826_050000.WAV',
+                            'UBNA_010/20220826_053000' : f'../data/audiomoth_recordings/recover-20220828/UBNA_010/20220826_053000.WAV',
+                            'UBNA_010/20220826_060000' : f'../data/audiomoth_recordings/recover-20220828/UBNA_010/20220826_060000.WAV',
+                            'UBNA_010/20220826_063000' : f'../data/audiomoth_recordings/recover-20220828/UBNA_010/20220826_063000.WAV',
                             'UBNA_010/20220826_070000' : f'../data/audiomoth_recordings/recover-20220828/UBNA_010/20220826_070000.WAV',
                             'UBNA_001/20220820_093000' : f'../data/audiomoth_recordings/recover-20220822/UBNA_001/20220820_093000.WAV',
                             'UBNA_002/20210910_030000': f'../data/audiomoth_recordings/recover-20210912/UBNA_002/20210910_030000.WAV'
                                 }
 
 EXAMPLE_FILES_to_DETECTIONS = {
+                            'UBNA_010/20220826_043000' : f'../data/raw/Central/bd2__Central_20220826_043000.csv',
+                            'UBNA_010/20220826_050000' : f'../data/raw/Central/bd2__Central_20220826_050000.csv',
+                            'UBNA_010/20220826_053000' : f'../data/raw/Central/bd2__Central_20220826_053000.csv',
+                            'UBNA_010/20220826_060000' : f'../data/raw/Central/bd2__Central_20220826_060000.csv',
+                            'UBNA_010/20220826_063000' : f'../data/raw/Central/bd2__Central_20220826_063000.csv',
                             'UBNA_010/20220826_070000' : f'../data/raw/Central/bd2__Central_20220826_070000.csv',
                             'UBNA_001/20220820_093000' : f'../data/raw/Telephone/bd2__Telephone_20220820_093000.csv',
                             'UBNA_002/20210910_030000': f'../batdetect2_outputs/recover-20210912/Foliage/bd2_20210910_030000.csv'
@@ -37,5 +47,5 @@ EXAMPLE_FILES_to_DETECTIONS = {
 EXAMPLE_FILES_from_LOCATIONS = {
                                 'Foliage' : 'UBNA_002/20210910_030000',
                                 'Telephone' : 'UBNA_001/20220820_093000',
-                                'Central' : 'UBNA_010/20220826_070000'
+                                'Central' : 'UBNA_010/20220826_050000'
                                 }
