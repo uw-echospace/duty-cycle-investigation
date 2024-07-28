e92799ed80cdd7505b53bb42322c431c1ddc8f2b
Aditya Krishna <adkris1002@gmail.com>
2024-07-27 17:14:57 -0700

    added conditional to debug

--- src/calls/call_classification.py
+++ src/calls/call_classification.py
@@ -176,8 +176,9 @@ def get_params_relevant_to_data_at_location(cfg):
 
     file_paths = get_file_paths(data_params)
     location_sum_df = pd.read_csv(f'{file_paths["SITE_folder"]}/{cfg["detector"]}__{data_params["type_tag"]}{data_params["site_tag"]}_2022.csv', low_memory=False, index_col=0)
-    location_sum_df.reset_index(inplace=True)
-    location_sum_df.rename({'index':'index_in_file'}, axis='columns', inplace=True)
+    if cfg['detector']=='bd2':
+        location_sum_df.reset_index(inplace=True)
+        location_sum_df.rename({'index':'index_in_file'}, axis='columns', inplace=True)
     location_sum_df.reset_index(inplace=True)
     location_sum_df.rename({'index':'index_in_summary'}, axis='columns', inplace=True)
     site_filepaths = relabel_drivenames_to_mirrors(location_sum_df['input_file'].copy().unique())
