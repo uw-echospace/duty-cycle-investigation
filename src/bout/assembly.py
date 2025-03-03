dd9ab6c2f2879f7a4f2977cfef8744931232fc97
Aditya Krishna <adkris1002@gmail.com>
2025-03-02 17:51:34 -0800

    updated notebooks with most recent progress; will move to make duty-cycle analysis work

--- src/bout/assembly.py
+++ src/bout/assembly.py
@@ -271,10 +271,7 @@ def get_bout_params_from_location(location_sum_df, data_params):
                 slow_process = clstr.calculate_exponential_coefficients(slow_process)
                 bci, misassigned_points = clstr.get_bci_from_slater_method(intervals_ms, survival, fast_process, slow_process)
                 nlin_results, misassigned_points_optim = clstr.get_bci_from_sibly_method(intervals_ms, survival, fast_process, slow_process)
-                if misassigned_points<=misassigned_points_optim:
-                    bout_params[f'{group}_bci'] = bci
-                else:
-                    bout_params[f'{group}_bci'] = nlin_results['bci']
+                bout_params[f'{group}_bci'] = bci
 
 
     return bout_params
